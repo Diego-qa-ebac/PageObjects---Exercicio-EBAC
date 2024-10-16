c27b9a00566aee32beeea92a247e22b512bfa946
Diego Portella <149001002+Diego-qa-ebac@users.noreply.github.com>
2024-10-16 15:56:59 -0300

    adicionando relatórios
    Cypress relatórios HTML

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+module.exports = defineConfig ({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-html-reporter/GenerateReport')(on, config)
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: "[name]-result",
+    html: true
+  }
 });
